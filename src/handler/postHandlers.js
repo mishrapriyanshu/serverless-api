624beec4da55a7b9b9affe13bb8e09c54778d2c2
mishrapriyanshu <cool.priyanshumishra@gmail.com>
2025-05-20 23:51:55 +0530

    implemented elastic search

--- src/handler/postHandlers.js
+++ src/handler/postHandlers.js
@@ -1,5 +1,6 @@
 const { connectToDatabase } = require('../../connection/mongo');
 const { createResponse } = require('../utils/utils');
+const { getClient } = require('../../connection/elasticsearch');
 
 exports.data = async (event) => {
   const body = event.body ? JSON.parse(event.body) : {};
@@ -16,3 +17,60 @@ exports.data = async (event) => {
     return createResponse('Database error', event, 500, { error: err.message });
   }
 };
+
+exports.createEsIndexAndMapping = async (event) => {
+  const body = event.body ? JSON.parse(event.body) : {};
+  const {esIndex, mapping} = body;
+  if (!esIndex) {
+    return createResponse('esIndex is required', event, 400);
+  }
+  if (!mapping) {
+    return createResponse('mapping is required', event, 400);
+  }
+  try {
+    const elasticsearchClient = await getClient();
+    console.log('Creating index:', esIndex);
+    const updateMappingResponse = await elasticsearchClient.indices.putMapping({
+      index: esIndex,
+      properties: mapping,
+    });
+    console.log(updateMappingResponse);
+    return createResponse(
+      'Mapping CREATED successfully!',
+      event,
+      201,
+      { received: body, mapping: updateMappingResponse }
+    );
+  } catch (err) {
+    return createResponse('Elastic search error', event, 500, { error: err.message });
+  }
+};
+
+exports.insertEsData = async (event) => {
+  const body = event.body ? JSON.parse(event.body) : {};
+  const {esIndex, data} = body;
+  if (!esIndex) {
+    return createResponse('esIndex is required', event, 400);
+  }
+  if (!data) {
+    return createResponse('data is required', event, 400);
+  }
+  const elasticsearchClient = await getClient();
+
+  const bulkIngestResponse = await elasticsearchClient.helpers.bulk({
+    index: esIndex,
+    datasource: data,
+    onDocument() {
+      return {
+        index: {},
+      };
+    }
+  });
+  console.log(bulkIngestResponse);
+  return createResponse(
+    'Data inserted successfully!',
+    event,
+    201,
+    { received: body, bulkIngestResponse }
+  );
+}
